7bea9ef9070d1a16f36f74f7a2bd68553e9b5a4f
dion <dyongtc@gmail.com>
2022-06-14 10:09:14 +0800

    Added stack

--- App.js
+++ App.js
@@ -8,10 +8,37 @@ import {
   View,
 } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-web";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+       <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+}
+function HomeScreen({ navigation }) {
+  return (
+    <TouchableOpacity
+        style={{ flex: 1, color: "#fff", alignItems: "center", justifyContent: "center" }} 
+        onPress={() => navigation.navigate("DetailsScreen" )}
+      >
+    <Text> Touch anywhere to go to DetailsScreen </Text>
+      </TouchableOpacity>
+    );
+}
+
+function DetailsScreen() {
   const [loading, setLoading] = useState(true);
   const [arrival, setArrival] = useState("");
+  const [busNo, setBusNo] = useState("");
+  const [busStopNo, setBusStopNo] = useState("");
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
   function loadBusStopData() {
@@ -20,15 +47,20 @@ export default function App() {
         return response.json();
       })
       .then((responseData) => {
-        console.log("Original data: ");
-        console.log(responseData);
+        // console.log("Original data: ");
+        // console.log(responseData);
         const myBus = responseData.services.filter(
           (item) => item.no ==="155"
         )[0];
-        console.log("My bus: ");
-        console.log(myBus.next.time);
-        setArrival(myBus.next.time);
-        console.log(myBus);
+        // console.log("My bus: ");
+        // console.log(myBus.next.time);
+        setBusNo(myBus.no);
+        setBusStopNo(myBus.next.stopNo);
+        const duration_ms=myBus.next.duration_ms;
+        console.log(duration_ms);
+        const duration_mins= Math.floor(duration_ms/60/1000);
+        setArrival(`${duration_mins} mins`);
+
         setLoading(false);
       });
   }
@@ -40,8 +72,13 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}> Bus: </Text>
+      <Text style={styles.subtitle}>
+        {" "}
+        {loading ? <ActivityIndicator color="blue" size="large"/> : busNo}
+      </Text>
       <Text style={styles.title}> Bus arrival time: </Text>
-      <Text style={styles.arrivalTime}>
+      <Text style={styles.subtitle}>
         {" "}
         {loading ? <ActivityIndicator color="blue" size="large"/> : arrival}
       </Text>
@@ -61,7 +98,11 @@ const styles = StyleSheet.create({
     fontSize: 100,
   },
   title: {
-    fontSize: 50,
+    fontSize: 40,
+    marginVertical: 20,
+  },
+  subtitle: {
+    fontSize: 20,
     marginVertical: 20,
   },
   button: {
